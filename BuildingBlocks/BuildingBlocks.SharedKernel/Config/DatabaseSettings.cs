f9e406377b8d96e8da0c38ce593d898ee5012a8a
glorylaflare <marcelojunior_@outlook.com.br>
2025-10-31 01:42:52 -0300

    feat: adição da nova classe DatabaseSettings

---
+++ BuildingBlocks/BuildingBlocks.SharedKernel/Config/DatabaseSettings.cs
@@ -0,0 +1,30 @@
+﻿namespace BuildingBlocks.SharedKernel.Config;
+
+public class DatabaseSettings
+{
+    public string Host { get; } = "sqlserver";
+    public int Port { get; } = 1433;
+    public required string Database { get; set; } 
+    public string Username { get; } = "sa";
+    public string Password { get; } = "yourStrong(!)Password";
+    public bool TrustServerCertificate { get; } = true;
+    public bool EnableConnectionPooling { get; } = true;
+
+    public int MinPoolSize { get; } = 0;
+    public int MaxPoolSize { get; } = 50;
+
+    public string ToConnectionString()
+    {
+        var pooling = EnableConnectionPooling ? 
+            $"Min Pool Size={MinPoolSize};Max Pool Size={MaxPoolSize};" 
+            : string.Empty;
+
+        return 
+            $"Server={Host},{Port};" +
+            $"Database={Database};" +
+            $"User Id={Username};Password={Password};" +
+            $"TrustServerCertificate={TrustServerCertificate};" +
+            $"{pooling}" +
+            $"MultipleActiveResultSets=true;";
+    }
+}
