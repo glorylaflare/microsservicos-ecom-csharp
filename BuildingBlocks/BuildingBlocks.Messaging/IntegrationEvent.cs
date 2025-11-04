8411b66ef05bbf9c99067614d66d7c5b6bb02b4a
glorylaflare <marcelojunior_@outlook.com.br>
2025-11-04 20:28:58 -0300

    feat: adição do correlationId a classe pai de integração dos eventos

--- BuildingBlocks/BuildingBlocks.Messaging/IntegrationEvent.cs
+++ BuildingBlocks/BuildingBlocks.Messaging/IntegrationEvent.cs
@@ -1,7 +1,15 @@
-﻿namespace BuildingBlocks.Messaging;
+﻿using System.Diagnostics;
+
+namespace BuildingBlocks.Messaging;
 
 public abstract record IntegrationEvent
 {
-    public Guid CorrelationId { get; } = Guid.NewGuid();
-    public DateTime OccurredAt { get; } = DateTime.UtcNow;
+    public Guid Id { get; init; } = Guid.NewGuid();
+    public DateTime OccurredAt { get; init; } = DateTime.UtcNow;
+    public string? CorrelationId { get; init; }
+    
+    protected IntegrationEvent(string correlationId)
+    {
+        CorrelationId = correlationId ?? Activity.Current?.TraceId.ToString();
+    }
 }
