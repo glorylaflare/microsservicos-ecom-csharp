18e8a10ec160b9f6e72d9f019a3cf2f7e42ab499
glorylaflare <marcelojunior_@outlook.com.br>
2025-11-04 20:33:15 -0300

    refactor: removendo configurações inúteis do reverseproxy

---
+++ ApiGateways/Extensions/ReverseProxyExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiGateways.Extensions;
+
+public static class ReverseProxyExtensions
+{
+    public static IServiceCollection AddReverseProxyServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddReverseProxy()
+            .LoadFromConfig(configuration.GetSection("ReverseProxy"));
+
+        return services;
+    }
+}
