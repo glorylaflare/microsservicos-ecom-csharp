c6f78c3ae93309e000acedfe4df6a40a5a9f11d4
glorylaflare <marcelojunior_@outlook.com.br>
2025-11-25 15:23:57 -0300

    feat: adição dos testes de integração para o serviço de usuário/autenticação

---
+++ Services/User/tests/User.IntegrationTests/Fixture/DatabaseFixture.cs
@@ -0,0 +1,35 @@
+﻿using BuildingBlocks.SharedKernel.Config;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using User.Infra.Data.Context;
+
+namespace User.IntegrationTests.Fixture;
+
+public class DatabaseFixture : IDisposable
+{
+    public WriteDbContext _context { get; private set; }
+    private readonly string _databaseName = Guid.NewGuid().ToString();
+
+    public DatabaseFixture()
+    {
+        var options = new DbContextOptionsBuilder<WriteDbContext>()
+            .UseInMemoryDatabase(_databaseName)
+            .Options;
+
+        _context = new WriteDbContext(
+            options,
+            databaseSettings: Options.Create(new DatabaseSettings
+            {
+                Host = "localhost",
+                Database = _databaseName
+            }));
+
+        _context.Database.EnsureCreated();
+    }
+
+    public void Dispose()
+    {
+        _context.Database.EnsureDeleted();
+        _context.Dispose();
+    }
+}
---
+++ Services/User/tests/User.IntegrationTests/Infra/Repositories/UserRepositoryTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using User.Infra.Data.Repositories;
+using User.IntegrationTests.Fixture;
+
+namespace User.IntegrationTests.Infra.Repositories;
+
+[Collection("Database Collection")]
+public class UserRepositoryTests
+{
+    private readonly DatabaseFixture _fixture;
+    private Domain.Models.User _user = new (
+        "Auth0|1234567890",
+        "testuser",
+        "test@email.com"
+    );
+    private readonly UserRepository _repository;
+
+    public UserRepositoryTests(DatabaseFixture fixture)
+    {
+        _fixture = fixture;
+        _repository = new UserRepository(_fixture._context);
+    }
+
+    [Fact]
+    public async Task AddAsync_WhenValid_ShouldCreateUser()
+    {
+        //Act
+        await _repository.AddAsync(_user);
+        await _repository.SaveChangesAsync();
+        var result = await _repository.GetByIdAsync(_user.Id);
+        //Assert
+        result.Should().NotBeNull();
+        result.Email.Should().Be(_user.Email);
+    }
+}
