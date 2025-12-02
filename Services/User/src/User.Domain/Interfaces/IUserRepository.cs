51bff77847a9651d32cb941e9e1cc382c0cd5bc0
glorylaflare <marcelojunior_@outlook.com.br>
2025-12-02 20:57:36 -0300

    feat: adição do método GetByEmailAsync para o contexto de Write do Repositório #2

--- Services/User/src/User.Domain/Interfaces/IUserRepository.cs
+++ Services/User/src/User.Domain/Interfaces/IUserRepository.cs
@@ -4,5 +4,6 @@ public interface IUserRepository
 {
     Task AddAsync(Models.User user);
     Task<Models.User?> GetByIdAsync(int userId);
+    Task<Models.User?> GetByEmailAsync(string email);
     Task SaveChangesAsync();
 }
