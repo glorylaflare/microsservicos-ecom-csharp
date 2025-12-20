5180e024f8d8e42f0b9cc9baddfadd8442e6e404
glorylaflare <marcelojunior_@outlook.com.br>
2025-12-19 22:21:24 -0300

    feat: ajustes na publicação do evento de Order no serviço de Stock #3

---
+++ Services/Stock/src/Stock.Application/Commands/OrderRequestCommand.cs
@@ -0,0 +1,6 @@
+﻿using BuildingBlocks.Contracts;
+using MediatR;
+
+namespace Stock.Application.Commands;
+
+public record OrderRequestCommand(int OrderId, List<OrderItemDto> Items) : IRequest<Unit>;
