c8a33036e930265864ea6308ffffcc8ea2ae3269
Marcelo Gonçalves <marcelojunior_@outlook.com.br>
2026-01-06 07:37:41 -0300

    feat: add EventBus configuration and Payment entity with status management #11

---
+++ Services/Payment/src/Payment.Application/Consumer/StockReservationResultConsumer.cs
@@ -0,0 +1,12 @@
+﻿using BuildingBlocks.Contracts.Events;
+using BuildingBlocks.Messaging;
+
+namespace Payment.Application.Consumer;
+
+public class StockReservationResultConsumer : IIntegrationEventHandler<StockReservationResultEvent>
+{
+    public Task HandleAsync(StockReservationResultEvent @event)
+    {
+        throw new NotImplementedException();
+    }
+}
