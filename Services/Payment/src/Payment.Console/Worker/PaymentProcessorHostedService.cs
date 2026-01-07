19617756894b82d983c3ef8c428458a83929d823
glorylaflare <marcelojunior_@outlook.com.br>
2026-01-07 16:46:49 -0300

    feat: add initial Payment.Console project structure #24

---
+++ Services/Payment/src/Payment.Console/Worker/PaymentProcessorHostedService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Hosting;
+using Payment.Console.Extensions;
+
+namespace Payment.Console.Worker;
+
+public class PaymentProcessorHostedService : BackgroundService
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public PaymentProcessorHostedService(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await _serviceProvider.ConfigureEventBus(stoppingToken);
+
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
