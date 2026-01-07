a4772f20bea68fab5999b231768181b9854acdda
glorylaflare <marcelojunior_@outlook.com.br>
2026-01-07 16:46:49 -0300

    feat: add initial Payment.Console project structure #24

---
+++ Services/Payment/src/Payment.Console/Extensions/ValidatorExtensions.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using Payment.Application.Commands;
+
+namespace Payment.Console.Extensions;
+
+public static class ValidatorExtensions
+{
+    public static IServiceCollection AddValidators(this IServiceCollection services)
+    {
+        services.AddScoped<IValidator<CreatePaymentCommand>, CreatePaymentCommandValidator>();
+
+        return services;
+    }
+}
---
+++ Services/Payment/src/Payment.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Payment.Application.Commands;
+using Payment.Console.Extensions;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddMediatR(cfg =>
+    cfg.RegisterServicesFromAssembly(typeof(CreatePaymentCommand).Assembly));
+
+builder.Services.AddValidators();
+
+var host = builder.Build();
+
+await host.RunAsync();
