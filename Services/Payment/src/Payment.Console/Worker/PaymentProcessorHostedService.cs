9356a92603d480f267df1d9c4cb944a46b9e423e
Marcelo Gonçalves <marcelojunior_@outlook.com.br>
2026-01-14 18:58:06 -0300

    feat: remove unused IConfiguration dependency from PaymentProcessorHostedService

--- Services/Payment/src/Payment.Console/Worker/PaymentProcessorHostedService.cs
+++ Services/Payment/src/Payment.Console/Worker/PaymentProcessorHostedService.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Hosting;
 using Payment.Console.Extensions;
 
 namespace Payment.Console.Worker;
@@ -7,12 +6,10 @@ namespace Payment.Console.Worker;
 public class PaymentProcessorHostedService : BackgroundService
 {
     private readonly IServiceProvider _serviceProvider;
-    private readonly IConfiguration _configuration;
 
-    public PaymentProcessorHostedService(IServiceProvider serviceProvider, IConfiguration configuration)
+    public PaymentProcessorHostedService(IServiceProvider serviceProvider)
     {
         _serviceProvider = serviceProvider;
-        _configuration = configuration;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
