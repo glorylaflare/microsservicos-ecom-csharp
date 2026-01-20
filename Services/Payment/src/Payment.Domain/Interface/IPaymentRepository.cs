a4d97e0703feb517961281b5bde99f6aa6f744ad
Marcelo Gonçalves <marcelojunior_@outlook.com.br>
2026-01-19 22:58:51 -0300

    feat: create IPaymentRepository interface and Payment model with status management

---
+++ Services/Payment/src/Payment.Domain/Interface/IPaymentRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Payment.Domain.Interface;
+
+public interface IPaymentRepository
+{
+    Task<Models.Payment?> GetByIdAsync(int orderId);
+    Task AddAsync(Models.Payment payment);
+    void Update(Models.Payment payment);
+    Task SaveChangesAsync();
+}
