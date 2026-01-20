aa1883a502312b881145eb22dc6df991ee5db5a9
Marcelo Gonçalves <marcelojunior_@outlook.com.br>
2026-01-19 22:57:37 -0300

    feat: add PaymentCreatedData and PaymentCreatedEvent records for payment processing

---
+++ BuildingBlocks/BuildingBlocks.Contracts/Events/PaymentCreatedEvent.cs
@@ -0,0 +1,12 @@
+﻿using BuildingBlocks.Contracts.Datas;
+using BuildingBlocks.Messaging;
+using System.Text.Json.Serialization;
+
+namespace BuildingBlocks.Contracts.Events;
+
+public record PaymentCreatedEvent : IntegrationEvent<PaymentCreatedData>
+{
+    [JsonConstructor]
+    public PaymentCreatedEvent(PaymentCreatedData data)
+        : base(data) { }
+}
