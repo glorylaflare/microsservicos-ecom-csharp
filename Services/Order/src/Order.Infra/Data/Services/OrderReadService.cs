a8482a7c85fdfedbd042d67e09551d9a35cb6fe1
Marcelo Gonçalves <marcelojunior_@outlook.com.br>
2026-01-24 08:01:12 -0300

    feat: refactor OrderReadService to use MongoDB for order retrieval #13

--- Services/Order/src/Order.Infra/Data/Services/OrderReadService.cs
+++ Services/Order/src/Order.Infra/Data/Services/OrderReadService.cs
@@ -1,68 +1,26 @@
-using BuildingBlocks.Infra.ReadModels;
+using BuildingBlocks.Infra.MongoReadModels;
 using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver;
 using Order.Application.Interfaces;
 using Order.Infra.Data.Context;
 namespace Order.Infra.Data.Services;
 
 public class OrderReadService : IOrderReadService
 {
-    private readonly DbSet<OrderReadModel> _orders;
-    private readonly DbSet<OrderItemReadModel> _orderItems;
+    private readonly IMongoCollection<OrderReadModel> _orders;
+
     public OrderReadService(ReadDbContext context)
     {
         _orders = context.Orders;
-        _orderItems = context.OrderItems;
     }
+
     public async Task<OrderReadModel?> GetByIdAsync(int orderId)
     {
-        var result = await _orders.Where(o => o.Id == orderId)
-            .Select(o => new OrderReadModel
-            {
-                Id = o.Id,
-                Status = o.Status,
-                TotalAmount = o.TotalAmount,
-                CreatedAt = o.CreatedAt,
-                UpdatedAt = o.UpdatedAt
-            })
-            .AsNoTracking()
-            .FirstOrDefaultAsync();
-        if (result is null) return null;
-        result.Items = await _orderItems.Where(oi => oi.OrderId == orderId)
-            .Select(oi => new OrderItemReadModel
-            {
-                OrderId = oi.OrderId,
-                ProductId = oi.ProductId,
-                Quantity = oi.Quantity
-            })
-            .AsNoTracking()
-            .ToListAsync();
-        return result;
+        return await _orders.Find(o => o.Id == orderId).FirstOrDefaultAsync();
     }
-    public async Task<IEnumerable<OrderReadModel>> GetAllAsync()
+
+    public async Task<IReadOnlyList<OrderReadModel>> GetAllAsync()
     {
-        var orders = await _orders.Select(o => new OrderReadModel
-        {
-            Id = o.Id,
-            Status = o.Status,
-            TotalAmount = o.TotalAmount,
-            CreatedAt = o.CreatedAt,
-            UpdatedAt = o.UpdatedAt
-        })
-            .AsNoTracking()
-            .ToListAsync();
-        var items = await _orderItems.Select(oi => new OrderItemReadModel
-        {
-            OrderId = oi.OrderId,
-            ProductId = oi.ProductId,
-            Quantity = oi.Quantity
-        })
-            .AsNoTracking()
-            .ToListAsync();
-        foreach (var order in orders)
-        {
-            order.Items = items.Where(i => i.OrderId == order.Id)
-                .ToList();
-        }
-        return orders;
+        return await _orders.Find(_ => true).ToListAsync();
     }
 }
