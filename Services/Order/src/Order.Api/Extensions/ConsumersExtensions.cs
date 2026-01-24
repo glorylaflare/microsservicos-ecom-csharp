b7d747f473a79bf5845bb15a41a20c00fbff4258
Marcelo Gonçalves <marcelojunior_@outlook.com.br>
2026-01-24 08:32:21 -0300

    refactor: change consumer registration from Scoped to Transient in ConsumersExtensions

--- Services/Order/src/Order.Api/Extensions/ConsumersExtensions.cs
+++ Services/Order/src/Order.Api/Extensions/ConsumersExtensions.cs
@@ -6,11 +6,11 @@ public static class ConsumersExtensions
 {
     public static IServiceCollection AddConsumers(this IServiceCollection services)
     {
-        services.AddScoped<StockReservationResultConsumer>();
+        services.AddTransient<StockReservationResultConsumer>();
 
         #region MongoDb projections
-        services.AddScoped<OrderCreatedProjector>();
-        services.AddScoped<OrderUpdatedProjector>();
+        services.AddTransient<OrderCreatedProjector>();
+        services.AddTransient<OrderUpdatedProjector>();
         #endregion
 
         return services;
--- Services/Payment/src/Payment.Api/Extensions/ConsumersExtensions.cs
+++ Services/Payment/src/Payment.Api/Extensions/ConsumersExtensions.cs
@@ -5,7 +5,7 @@ public static class ConsumersExtensions
 {
     public static IServiceCollection AddConsumers(this IServiceCollection services)
     {
-        services.AddScoped<StockReservationResultConsumer>();
+        services.AddTransient<StockReservationResultConsumer>();
         return services;
     }
 }
