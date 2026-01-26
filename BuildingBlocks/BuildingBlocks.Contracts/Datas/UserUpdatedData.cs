82467e536b1e85216826a5dbc79fbe2005ec2b34
Marcelo Gonçalves <marcelojunior_@outlook.com.br>
2026-01-26 18:59:31 -0300

    feat: add UserUpdatedData and UserUpdatedEvent for user update events #12

---
+++ BuildingBlocks/BuildingBlocks.Contracts/Datas/UserUpdatedData.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BuildingBlocks.Contracts.Datas;
+
+public record UserUpdatedData
+{
+    public string Id { get; init; }
+    public string Username { get; init; }
+    public string Email { get; init; }
+
+    [JsonConstructor]
+    public UserUpdatedData(string id, string username, string email)
+    {
+        Id = id;
+        Username = username;
+        Email = email;
+    }
+}
---
+++ BuildingBlocks/BuildingBlocks.Contracts/MongoEvents/UserUpdatedEvent.cs
@@ -0,0 +1,12 @@
+﻿using BuildingBlocks.Contracts.Datas;
+using BuildingBlocks.Messaging;
+using System.Text.Json.Serialization;
+
+namespace BuildingBlocks.Contracts.MongoEvents;
+
+public record UserUpdatedEvent : IntegrationEvent<UserUpdatedData>
+{
+    [JsonConstructor]
+    public UserUpdatedEvent(UserUpdatedData data)
+        : base(data) { }
+}
