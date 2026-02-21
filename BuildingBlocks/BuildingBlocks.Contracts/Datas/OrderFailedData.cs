4557d066728ddee72889715f44372bbb10946348
Marcelo Gonçalves <marcelojunior_@outlook.com.br>
2026-02-21 11:23:42 -0300

    feat: add OrderFailedData and OrderFailedEvent classes for handling order failure events #40

---
+++ BuildingBlocks/BuildingBlocks.Contracts/Datas/OrderFailedData.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BuildingBlocks.Contracts.Datas;
+
+public record OrderFailedData
+{
+    public List<OrderItemDto> Items { get; set; }
+    public string Reason { get; set; } = string.Empty;
+    public string Email { get; set; }
+
+    [JsonConstructor]
+    public OrderFailedData(List<OrderItemDto> items, string reason, string email)
+    {
+        Items = items;
+        Reason = reason;
+        Email = email;
+    }
+}
---
+++ BuildingBlocks/BuildingBlocks.Contracts/Events/OrderFailedEvent.cs
@@ -0,0 +1,12 @@
+﻿using BuildingBlocks.Contracts.Datas;
+using BuildingBlocks.Messaging;
+using System.Text.Json.Serialization;
+
+namespace BuildingBlocks.Contracts.Events;
+
+public record OrderFailedEvent : IntegrationEvent<OrderFailedData>
+{
+    [JsonConstructor]
+    public OrderFailedEvent(OrderFailedData data)
+        : base(data) { }
+}
