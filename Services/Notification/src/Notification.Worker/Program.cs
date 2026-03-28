9bab8743a903da1e63d00c3aa246f1e52ac2df52
Marcelo Gonçalves <marcelojunior_@outlook.com.br>
2026-03-27 23:43:13 -0300

    refactor: update Program.cs to use Host.CreateApplicationBuilder; enhance service configuration

--- Services/Notification/src/Notification.Worker/Program.cs
+++ Services/Notification/src/Notification.Worker/Program.cs
@@ -1,8 +1,8 @@
 ﻿using BuildingBlocks.Messaging.Config;
 using BuildingBlocks.Messaging.Extensions;
+using BuildingBlocks.Observability.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
 using Notification.Application.Commands;
 using Notification.Application.Interfaces;
 using Notification.Infra.Configurations;
@@ -10,31 +10,37 @@ using Notification.Infra.Services;
 using Notification.Worker.Configurations;
 using Resend;
 
-var host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices((context, services) =>
-    {
-        services.Configure<RabbitMQSettings>(
-            context.Configuration.GetSection("RabbitMqSettings"));
+var builder = Host.CreateApplicationBuilder(new HostApplicationBuilderSettings
+{
+    Args = args,
+    ContentRootPath = AppContext.BaseDirectory
+});
 
-        services.Configure<ResendSettings>(
-            context.Configuration.GetSection("ResendSettings"));
-        services.AddHttpClient<ResendClient>();
-        services.AddScoped<IEmailSender, ResendEmailSender>();
-        services.AddTransient<IResend, ResendClient>();
+builder.Services.Configure<RabbitMQSettings>(
+    builder.Configuration.GetSection("RabbitMqSettings"));
 
-        services.AddMediatR(cfg =>
-            cfg.RegisterServicesFromAssembly(typeof(OrderCompletedEmailCommand).Assembly));
+builder.Services.Configure<ResendSettings>(
+    builder.Configuration.GetSection("ResendSettings"));
+builder.Services.AddHttpClient<ResendClient>();
+builder.Services.AddScoped<IEmailSender, ResendEmailSender>();
+builder.Services.AddScoped<ITemplateRenderer, TemplateRenderer>();
+builder.Services.AddTransient<IResend, ResendClient>();
 
-        services.AddOptions();
-        services.Configure<ResendClientOptions>(o =>
-        {
-            o.ApiToken = context.Configuration["ResendSettings:ApiKey"]!;
-        });
+builder.Services.AddMediatR(cfg =>
+    cfg.RegisterServicesFromAssembly(typeof(OrderCompletedEmailCommand).Assembly));
 
-        services.AddConsumers();
-        services.AddEventBus();
-    })
-    .Build();
+builder.Services.AddCustomLogging(builder.Configuration);
+
+builder.Services.AddOptions();
+builder.Services.Configure<ResendClientOptions>(o =>
+{
+    o.ApiToken = builder.Configuration["ResendSettings:ApiKey"] ?? string.Empty;
+});
+
+builder.Services.AddConsumers();
+builder.Services.AddEventBus();
+
+var host = builder.Build();
 
 await DependencyInjection.SubscribeEventsAsync(host.Services);
 
