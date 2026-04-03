f7d3653153c18d1fd5a599b6228abe5d33ae46c0
Marcelo Gonçalves <marcelojunior_@outlook.com.br>
2026-04-03 11:35:07 -0300

    feat: implement user created email notification system with command and event handling #73

---
+++ Services/Notification/src/Notification.Application/Commands/UserCreated/UserCreatedEmailCommandHandler.cs
@@ -0,0 +1,54 @@
+﻿using MediatR;
+using Notification.Application.Commands.OrderCompleted;
+using Notification.Application.Interfaces;
+using Notification.Domain.Exceptions;
+using Notification.Domain.Models;
+using Serilog;
+
+namespace Notification.Application.Commands.UserCreated;
+
+public class UserCreatedEmailCommandHandler : IRequestHandler<UserCreatedEmailCommand, Unit>
+{
+    private readonly IEmailSender _emailSender;
+    private readonly ITemplateRenderer _templateRenderer;
+    private readonly ILogger _logger;
+
+    public UserCreatedEmailCommandHandler(IEmailSender emailSender, ITemplateRenderer templateRenderer)
+    {
+        _emailSender = emailSender;
+        _templateRenderer = templateRenderer;
+        _logger = Log.ForContext<UserCreatedEmailCommandHandler>();
+    }
+
+    public async Task<Unit> Handle(UserCreatedEmailCommand request, CancellationToken ct)
+    {
+        _logger.Information("[INFO] Handling {CommandName}", nameof(UserCreatedEmailCommand));
+
+        try
+        {
+            var html = await _templateRenderer.RenderAsync<UserCreatedEmailCommand>(
+                new Dictionary<string, string>
+                {
+                    ["Username"] = request.Username,
+                    ["Email"] = request.Email,
+                }
+            );
+
+            await _emailSender.SendAsync(new Message
+            {
+                To = request.Email,
+                Subject = "Bem-vindo(a) ao nosso serviço de E-Commerce",
+                Body = html
+            });
+
+            _logger.Information("[INFO] User created email sent successfully");
+
+            return Unit.Value;
+        }
+        catch (EmailSendException ex)
+        {
+            _logger.Error(ex, "[ERROR] Failed to send user created email");
+            throw;
+        }
+    }
+}
---
+++ Services/Notification/src/Notification.Application/Consumers/UserCreatedEmailRequestConsumer.cs
@@ -0,0 +1,26 @@
+﻿using BuildingBlocks.Contracts.Events;
+using BuildingBlocks.Messaging;
+using MediatR;
+using Notification.Application.Commands.UserCreated;
+using Serilog;
+
+namespace Notification.Application.Consumers;
+
+public class UserCreatedEmailRequestConsumer : IIntegrationEventHandler<UserCreatedEmailRequestEvent>
+{
+    private readonly ILogger _logger;
+    private readonly IMediator _mediator;
+
+    public UserCreatedEmailRequestConsumer(IMediator mediator)
+    {
+        _mediator = mediator;
+        _logger = Log.ForContext<OrderEmailRequestConsumer>();
+    }
+
+    public async Task HandleAsync(UserCreatedEmailRequestEvent @event)
+    {
+        _logger.Information("[INFO] Handling {EventName}", nameof(UserCreatedEmailRequestEvent));
+
+        await _mediator.Send(new UserCreatedEmailCommand(@event.Data.Email, @event.Data.Username));
+    }
+}
